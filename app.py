77ad8fd8871ff9044e79a826741a9550774aa4c8
Thomas Escrihuela <thomas.escrihuela@cerema.fr>
2022-11-14 14:46:03 +0100

    test heroku

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import missing11
 # Serveur
 server = flask.Flask(__name__)
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, server=server, title='Missing11 choice', external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, server=server, title='Missing11 choice', external_stylesheets=external_stylesheets, url_base_pathname='/missing11/')
 app.config.suppress_callback_exceptions = True
 
 df = pd.read_csv("missing11.csv")
